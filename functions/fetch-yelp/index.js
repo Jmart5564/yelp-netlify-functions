703679b7de268f4feca482cd80fe69cfd2d00f54
Jmart5564 <Jmart5564@gmail.com>
2022-10-11 12:33:00 -0700

    condensed code to clean it up

--- functions/fetch-yelp/index.js
+++ functions/fetch-yelp/index.js
@@ -3,8 +3,7 @@ require('dotenv').config({ path: `.env.development.local` });
 
 const handler = async (event) => {
 
-  const { zip } = event.queryStringParameters;
-  const { search } = event.queryStringParameters;
+  const { zip, search } = event.queryStringParameters;
 
   try {
     const resp = await fetch(`https://api.yelp.com/v3/businesses/search?categories=restaurants&location=${zip}&term=${search}`,
